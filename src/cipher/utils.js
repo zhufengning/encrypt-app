fd25b035d377cda808c61ae8aa1681d519cfae7b
zhufengning <z@zhufn.fun>
2023-11-20 19:31:30 +0800

    init project

---
+++ src/cipher/utils.js
@@ -0,0 +1,3 @@
+export function str2ArrayBuffer() {
+  return "hello from utils"
+}
---
+++ src/renderer/src/components/Versions.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { reactive } from 'vue'
+import { str2ArrayBuffer } from '../../../cipher/utils'
+const versions = reactive({ ...window.electron.process.versions })
+const hello = str2ArrayBuffer()
+</script>
+
+<template>
+  <ul class="versions">
+    <li class="electron-version">Electron v{{ versions.electron }}</li>
+    <li class="chrome-version">Chromium v{{ versions.chrome }}</li>
+    <li class="node-version">Node v{{ versions.node }}</li>
+    <li class="v8-version">V8 v{{ versions.v8 }}</li>
+    {{ hello }}
+  </ul>
+</template>
