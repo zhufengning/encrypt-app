83297e42bc2a28ac9b69cefd0de5f61ea06d9079
zhufengning <z@zhufn.fun>
2023-11-21 18:02:51 +0800

    fix: 打包后启动时显示的仍是首页

--- src/renderer/src/router/index.js
+++ src/renderer/src/router/index.js
@@ -1,19 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import a from '../a.vue'
 import b from '../b.vue'
 import c from '../c.vue'
 
 const routes = [
-    { path: '/1', component: a },
-    { path: '/2', component: b },
-    { path: '/3', component: c },
-    { path: '/',  redirect: '/1' },
+  { path: '/1', component: a },
+  { path: '/2', component: b },
+  { path: '/3', component: c },
+  { path: '/', redirect: '/1' },
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHashHistory(),
+  routes
 })
 
 export default router
