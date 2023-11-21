b857cb63b5417e5a373fad2df48498656100a7d0
zhufengning <z@zhufn.fun>
2023-11-21 17:33:30 +0800

    fix: router

--- src/renderer/src/index.vue
+++ src/renderer/src/index.vue
@@ -1,29 +1,34 @@
 <script setup>
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 var value = ref(0)
+const router = useRouter()
+function jmp(s) {
+  router.push(s);
+}
 </script>
 
 <template>
-    <router-view></router-view>
+  <router-view></router-view>
 
 
-    <v-layout class="overflow-visible" style="height: 56px;">
-        <v-bottom-navigation v-model="value" active color="primary">
-            <v-btn @click="this.$router.push('/');">
-                <v-icon>mdi-history</v-icon>
-                经典密码
-            </v-btn>
+  <v-layout class="overflow-visible" style="height: 56px;">
+    <v-bottom-navigation v-model="value" active color="primary">
+      <v-btn @click="jmp('/')">
+        <v-icon>mdi-history</v-icon>
+        经典密码
+      </v-btn>
 
-            <v-btn @click="this.$router.push('/about');">
-                <v-icon>mdi-heart</v-icon>
+      <v-btn @click="jmp('/about');">
+        <v-icon>mdi-heart</v-icon>
 
-                现代密码
-            </v-btn>
+        现代密码
+      </v-btn>
 
-            <v-btn>
-                <v-icon>mdi-map-marker</v-icon>
-                联机模式
-            </v-btn>
-        </v-bottom-navigation>
-    </v-layout>
+      <v-btn>
+        <v-icon>mdi-map-marker</v-icon>
+        联机模式
+      </v-btn>
+    </v-bottom-navigation>
+  </v-layout>
 </template>
